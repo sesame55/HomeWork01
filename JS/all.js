dadc1b15c61c173c05e040033bba39223132b12e
sesame55 <mushroom0505@gmail.com>
2020-03-06 00:58:08 +0800

    22
    22

--- JS/all.js
+++ JS/all.js
@@ -1,27 +1,30 @@
-$(function () {
-    // 先取得 #cart 及其 top 值
-    var $cart = $('#cart'),
-        _top = $cart.offset().top;
+$(document).ready(function () {
+    $(function () {
+        // 先取得 #cart 及其 top 值
+        var $cart = $('#cart'),
+            _top = $cart.offset().top;
 
-    // 當網頁捲軸捲動時
-    var $win = $(window).scroll(function () {
-        // 如果現在的 scrollTop 大於原本 #cart 的 top 時
-        if ($win.scrollTop() >= _top) {
-            // 如果 $cart 的座標系統不是 fixed 的話
-            if ($cart.css('position') != 'fixed') {
-                // 設定 #cart 的座標系統為 fixed
+        // 當網頁捲軸捲動時
+        var $win = $(window).scroll(function () {
+            // 如果現在的 scrollTop 大於原本 #cart 的 top 時
+            if ($win.scrollTop() >= _top) {
+                // 如果 $cart 的座標系統不是 fixed 的話
+                if ($cart.css('position') != 'fixed') {
+                    // 設定 #cart 的座標系統為 fixed
+                    $cart.css({
+                        position: 'fixed',
+                        top: 0
+                    });
+                }
+            } else {
+                // 還原 #cart 的座標系統為 absolute
                 $cart.css({
-                    position: 'fixed',
-                    top: 0
+                    position: 'absolute'
                 });
             }
-        } else {
-            // 還原 #cart 的座標系統為 absolute
-            $cart.css({
-                position: 'absolute'
-            });
-        }
+        });
     });
+
 });
 
 //來源：https://abgne.tw/jquery/apply-jquery/apple-store-scroll-fixed-shopping-list.html
